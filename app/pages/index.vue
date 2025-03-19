ae5a191659fbb540e9e5435d9f232d81fab610e0
AjaeOnTheRun <syahrul.azrai@gmail.com>
2025-03-19 20:53:56 +0800

    update links

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="flex flex-col items-center justify-center gap-4 h-screen">
     <h1 class="font-bold text-2xl text-(--ui-primary)">
-      Nuxt UI v3
+      Dynalis Excel Sheet Interpreter
     </h1>
 
     <div class="flex items-center gap-2">
       <UButton
-        label="Documentation"
+        label="Enter"
         icon="i-lucide-square-play"
-        to="https://ui3.nuxt.dev/getting-started/installation/nuxt"
-        target="_blank"
+        to="/dashboard"
       />
 
       <UButton
@@ -17,7 +16,7 @@
         color="neutral"
         variant="outline"
         icon="i-simple-icons-github"
-        to="https://github.com/nuxt/ui"
+        to="https://github.com/AzraiHasan/dynalis/tree/master"
         target="_blank"
       />
     </div>
